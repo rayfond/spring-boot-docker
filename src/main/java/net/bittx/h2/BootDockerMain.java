4277a4f6a09d7ad0a4ad6e5a94c9a9b962be56f4
asin <codezone@163.com>
2019-10-22 18:36:13 +0800

    docker spring boot

---
+++ src/main/java/net/bittx/h2/BootDockerMain.java
@@ -0,0 +1,12 @@
+package net.bittx.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDockerMain {
+    public static void main(String[] args) {
+        SpringApplication.run(BootDockerMain.class, args);
+    }
+
+}
