090fe8e9372ec1cdbd89dd21e2fd5494fcee9877
asin <codezone@163.com>
2019-10-25 18:08:08 +0800

    stop.sh

---
+++ src/bin/stop.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+APP_NAME=@project.name@
+BUILD_JAR="@project.build.finalName@.jar"
+
+PID=$(ps -ef | grep "${BUILD_JAR}" | grep -v 'grep' | awk '{ print $2 }')
+
+if [ -z "${PID}" ]
+then
+    echo ${APP_NAME} is already stopped.
+else
+    echo "shutdown application ${APP_NAME} ..."
+    kill ${PID} &&  echo ${APP_NAME} stopped successfully.
+fi
