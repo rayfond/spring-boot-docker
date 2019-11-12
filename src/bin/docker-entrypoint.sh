6128e66e4c949cfd1fffa03dce2cefb97e86e9e0
asin <codezone@163.com>
2019-11-12 09:59:20 +0800

    feature: cmd param supported.

--- src/bin/docker-entrypoint.sh
+++ src/bin/docker-entrypoint.sh
@@ -1,21 +1,23 @@
 #!/bin/sh 
 set -oe pipefail
 
+CMD=start
+
 # allow the container to be started with `--user`
-if [ "$1" = 'start' ]; then
+if [ "$1" = ${CMD} ]; then
 
     DIRNAME=`which dirname`
     BIN_PATH=$( cd `${DIRNAME} $0`; pwd -P )
     APP_HOME=$(${DIRNAME} ${BIN_PATH})
-    . $BIN_PATH/env.sh docker
+    . "$BIN_PATH"/env.sh docker
 
     echo "Logging system starting now ..." >> ${APP_HOME}/logs/access.log
     # Continuously provide logs so that 'docker logs' can produce them
     tail -F "${APP_HOME}/logs/access.log" &
 
-    param=`echo $* | sed s/start//g`
+    param=`echo $* | sed s/${CMD}//g`
 
-    OPTS=$OPTS$param
+    OPTS=${OPTS}${param}
 
     echo "Param is:[${param}]"
     echo "java" ${OPTS} "-jar" ${APP_HOME}/boot/${BUILD_JAR}
