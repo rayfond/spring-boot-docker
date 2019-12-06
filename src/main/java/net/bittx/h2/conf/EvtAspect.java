601147fd0b716452fbaf49b06bd54cea07908ff8
asin <codezone@163.com>
2019-12-06 15:40:59 +0800

    feature: request params overwrite annotation properties.

--- src/main/java/net/bittx/h2/conf/EvtAspect.java
+++ src/main/java/net/bittx/h2/conf/EvtAspect.java
@@ -1,9 +1,13 @@
 package net.bittx.h2.conf;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.bittx.h2.anno.Evt;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -13,7 +17,10 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Aspect
@@ -22,16 +29,25 @@ import java.util.Arrays;
 public class EvtAspect {
     private static final Logger logger = LoggerFactory.getLogger(EvtAspect.class);
 
-    @Pointcut("execution(public * net.bittx..h2.controller..*.*(..))")
-    public void evt() {
-    }
+    private static final String expr = "execution(public * net.bittx..h2.controller..*.*(..)) " +
+            "&& @annotation(net.bittx.h2.anno.Evt)";
 
-    @Before("evt()")
+    //@Pointcut("execution(public * net.bittx..h2.controller..*.*(..))")
+    //@Pointcut("execution(public * net.bittx..h2.controller..*.*(..))&& @annotation(net.bittx.h2.anno.Evt)")
+    @Pointcut(expr)
+    public void evt() {}
+
+    // @Before("evt()")
     public void beforeAdvice(JoinPoint joinPoint) {
 
         // 得到参数列表: joinPoint.getArgs();
         // 得到目标对象: joinPoint.getTarget();
         // 得到方法名称: joinPoint.getSignature().getName();
+        // 得到方法名称:
+        //              Signature signature = joinPoint.getSignature();
+        //              MethodSignature methodSignature = (MethodSignature)signature;
+        //              Method targetMethod = methodSignature.getMethod();
+
         HttpServletRequest request = ((ServletRequestAttributes)
                 RequestContextHolder.getRequestAttributes())
                 .getRequest();
@@ -39,48 +55,56 @@ public class EvtAspect {
         int port = request.getRemotePort();
 
 
-            Class targetClass = joinPoint.getTarget().getClass();
+        Class targetClass = joinPoint.getTarget().getClass();
 
-            // 当前访问的类路径
-            String targetName = targetClass.getName();
-            // 当前访问的方法名称
-            String methodName = joinPoint.getSignature().getName();
+        // 当前访问的类路径(包含包名称)
+        String targetName = targetClass.getName();
+        // 当前访问的方法名称
+        String methodName = joinPoint.getSignature().getName();
 
-            // 获取类中的公共方法
-            //Class targetClass = Class.forName(targetName);
+        Signature signature = joinPoint.getSignature();
+        MethodSignature methodSignature = (MethodSignature)signature;
+        Method targetMethod = methodSignature.getMethod();
 
-            Method[] methods = targetClass.getMethods();
+        System.out.println(targetName);
+        System.out.println(methodName);
 
-            String params = "";
-            Object[] args = joinPoint.getArgs();
-            if (args != null && args.length > 0) {
-                params = Arrays.toString(args);
-            }
 
-            String logName = "";
-            String modelName = "";
+        // 获取类中的公共方法
+        //Class targetClass = Class.forName(targetName);
 
-            for (Method method : methods) {
-                if (method.getName().equals(methodName)) {
-                    Class[] clazz = method.getParameterTypes();
-                    if (clazz.length == args.length) {  // TODO: 可能需要使用Objects.deepEquals(method,args)
-                        Evt anno = method.getAnnotation(Evt.class);
-                        if (anno != null) {
-                            logName = anno.value();
-                            modelName = anno.code();
-                        }
-                        // 此处只是比较了参数的个数相同，可能需要使用Objects.deepEquals(method,args)
-                        break;
+        Method[] methods = targetClass.getMethods();
+
+        String params = "";
+        Object[] args = joinPoint.getArgs();
+        if (args != null && args.length > 0) {
+            params = Arrays.toString(args);
+        }
+
+        String logName = "";
+        String modelName = "";
+
+        for (Method method : methods) {
+            if (method.getName().equals(methodName)) {
+                Class[] clazz = method.getParameterTypes();
+                if (clazz.length == args.length) {  // TODO: 可能需要使用Objects.deepEquals(method,args)
+                    Evt anno = method.getAnnotation(Evt.class);
+                    if (anno != null) {
+                        logName = anno.value();
+                        modelName = anno.code();
                     }
+                    // 此处只是比较了参数的个数相同，可能需要使用Objects.deepEquals(method,args)
+                    break;
                 }
             }
+        }
 
-            // 通过类路径取得所有信息
-            logger.info("name: {}",logName);
-            logger.info("module: {}",modelName);
-            logger.info("param: {}",params);
-            logger.info("class: {}",targetName);
-            logger.info("method: {}", methodName);
+        // 通过类路径取得所有信息
+        logger.info("name: {}", logName);
+        logger.info("module: {}", modelName);
+        logger.info("param: {}", params);
+        logger.info("class: {}", targetName);
+        logger.info("method: {}", methodName);
 
     }
 
@@ -89,9 +113,51 @@ public class EvtAspect {
         System.out.println("after advice");
     }
 
-    @AfterReturning(pointcut = "execution(public * net.bittx..h2.controller..*.*(..))",returning = "returningValue")
+    @AfterReturning(
+            //pointcut = "execution(public * net.bittx..h2.controller..*.*(..))&& @annotation(net.bittx.h2.anno.Evt)",
+            pointcut = "evt()",
+            returning = "returningValue")
     public void afterReturning(JoinPoint jp, Object returningValue) {
-        //
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest req = attributes.getRequest();
+
+        Class targetClass = jp.getTarget().getClass();
+
+        Signature signature = jp.getSignature();
+        MethodSignature methodSignature = (MethodSignature)signature;
+        Method targetMethod = methodSignature.getMethod();
+        Object[] args = jp.getArgs();
+
+            /*log.info("Request::  url:{},    method:{},   IP:{}, classMethod:{}.{}  params{}",
+                    req.getRequestURL(),req.getMethod(),req.getRemoteHost(),
+                    signature.getDeclaringTypeName(),signature.getName(),
+                    Arrays.toString(jp.getArgs()));*/
+        String src = targetClass.getName() + "." +  signature.getName();
+
+        Evt be = targetMethod.getAnnotation(Evt.class);
+
+        // Annotation
+        String code = be.code();
+        String comment = be.value();
+
+        code = code.equals("")? args[0].toString(): code;
+        comment = comment.equals("") ? args[1].toString():comment;
+
+        Map<String, Object> payload = new HashMap<>();
+
+        payload.put("createDate", LocalDateTime.now());
+        payload.put("code", code);
+        payload.put("comment", comment);
+        payload.put("src",src);
+
+        ObjectMapper om = new ObjectMapper();
+        String rtn = null;
+        try {
+            rtn = om.writeValueAsString(payload);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        System.out.println(rtn);
     }
 
     @AfterThrowing(pointcut = "evt()", throwing = "e")
@@ -100,7 +166,7 @@ public class EvtAspect {
     }
 
     //@Around("execution(public * net.bittx..h2.controller..*.*(..))")
-    public void around(ProceedingJoinPoint pjp){
+    public void around(ProceedingJoinPoint pjp) {
         // 方法之前前置通知
         try {
             pjp.proceed();
--- src/main/java/net/bittx/h2/controller/A.java
+++ src/main/java/net/bittx/h2/controller/A.java
@@ -1,6 +1,8 @@
 package net.bittx.h2.controller;
 
 import net.bittx.h2.anno.Evt;
+import org.apache.ibatis.annotations.Param;
+import org.aspectj.lang.annotation.RequiredTypes;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,11 @@ public class A {
     @RequestMapping("d")
     public String anno2(){
         return "Test @Evt!";
+    }
+
+    @Evt
+    @RequestMapping("f")
+    public String f(String code, String value,int index){
+        return "Test @Evt with params!";
     }
 }
